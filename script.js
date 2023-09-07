322026888af971fa3b423b4260927f2def78f4a3
iaftabzaman <54945683+iaftabzaman@users.noreply.github.com>
2023-09-07 17:09:49 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// Get elements from the DOM
+
 const inputTextArea = document.getElementById('inputTextArea');
 const outputTextArea = document.getElementById('outputTextArea');
 const convertButton = document.getElementById('convertButton');
 const copyButton = document.getElementById('copyButton');
 const downloadLink = document.getElementById('downloadLink');
 
-// Convert text to CSV format
+
 function textToCSV(text) {
-    // Replace spaces with commas
+
     return text.replace(/ /g, ',');
 }
 
-// Convert button click event
+
 convertButton.addEventListener('click', () => {
     const inputText = inputTextArea.value;
     const csvText = textToCSV(inputText);
@@ -21,7 +21,7 @@ convertButton.addEventListener('click', () => {
     downloadLink.style.display = 'block';
 });
 
-// Copy button click event
+
 copyButton.addEventListener('click', () => {
     outputTextArea.select();
     document.execCommand('copy');
